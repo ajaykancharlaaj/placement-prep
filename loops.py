0556f8ea1709593429305033eaf7124feaa31571
AJAY KANCHARLA <143792755+ajaykancharlaaj@users.noreply.github.com>
2025-12-06 11:19:50 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+    i=0
+    if 1<=n<=20:
+        for i in range(0,n,1):
+            print(i*i)
---
+++ print_function.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+    j=1
+    if 1<=n<=150:
+        for i in range(1,n+1,1):
+            print(i,end="")
