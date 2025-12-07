3c555967b8189778a0645aad86e364705eeda352
AJAY KANCHARLA <143792755+ajaykancharlaaj@users.noreply.github.com>
2025-12-07 08:10:22 +0530

    Update write_a_function.py

--- write_a_function.py
+++ write_a_function.py
@@ -1,11 +1,12 @@
 def is_leap(year):
     leap =False
-    if year%400==0:
+    if year%400==0:  #if year divisible by 400 then it is a leap year
         return True 
-    elif year%100==0:
+    elif year%100==0:   #if year divisible by 100 then its not leap year
         return False
-    elif year%4==0:
+    elif year%4==0:    #if year divisible by 4 then it also a leap year
         return True
-    return leap
+    return leap   
 year = int(input())
+
 print(is_leap(year))
