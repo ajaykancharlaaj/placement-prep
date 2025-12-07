ce2749b8ffd1b2973497194277b096a139af02c4
AJAY KANCHARLA <143792755+ajaykancharlaaj@users.noreply.github.com>
2025-12-07 08:15:50 +0530

    Update print_function.py

--- print_function.py
+++ print_function.py
@@ -2,5 +2,6 @@ if __name__ == '__main__':
     n = int(input())
     j=1
     if 1<=n<=150:
-        for i in range(1,n+1,1):
-            print(i,end="")
+        for i in range(1,n+1,1): #this loop gives numbers form 1 to nth number
+            print(i,end="")    #it prints the numbers in a single row from 1 to nth number 
+
